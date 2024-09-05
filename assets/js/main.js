3ef6e51fe4278622e5599679528f4b0e1a560c19
yeffer <ycuesta@providenciacfi.com>
2024-09-05 16:07:20 -0500

    tarde 05 09 2024

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,67 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+
 const menuToggle = document.getElementById('menu-toggle');
 const menu = document.querySelector('#menu');
 const body = document.body;
 
-// Abre y cierra el menú hamburguesa
-menuToggle.addEventListener('click', (event) => {
-    // Prevenir que el clic en el botón hamburguesa cierre el menú
-    event.stopPropagation();
-    menuToggle.classList.toggle('active');
-    menu.classList.toggle('active');
-    
-});
 
-// Cierra el menú cuando se hace clic fuera de él
-body.addEventListener('click', (event) => {
-    // Verificar si el clic está fuera del menú y el botón hamburguesa
-    if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
-        menu.classList.remove('active');
-        menuToggle.classList.remove('active');
-    }
-});
 
-// funcion para mostrar los menus 
-// document.addEventListener('DOMContentLoaded', () => {
-//     // Selecciona todos los elementos <a> con la clase 'boton-menu'
-//     const botonMenus = document.querySelectorAll('.boton-menu');
-  
-//     botonMenus.forEach(boton => {
-//       boton.addEventListener('click', (event) => {
-//         event.preventDefault(); // Evita el comportamiento por defecto del <a>
+    window.addEventListener("load", function () {
+        const loader = document.getElementById("loader-page-container");
+        loader.classList.add("hidden");  // Aplicamos la clase para ocultar el loader
+    });
+
+    document.addEventListener("scroll", function() {
+        var iframe = document.querySelector("#panel-noticias iframe");
+        if (iframe && iframe.getBoundingClientRect().top < window.innerHeight) {
+          iframe.src = iframe.dataset.src;
+        }
+      });
+
+
+    // Abre y cierra el menú hamburguesa
+    menuToggle.addEventListener('click', (event) => {
+        // Prevenir que el clic en el botón hamburguesa cierre el menú
+        event.stopPropagation();
+        menuToggle.classList.toggle('active');
+        menu.classList.toggle('active');
         
-//         // Obtiene el <ul> que es el siguiente hermano del <a> clicado
-//         const subMenu = boton.nextElementSibling;
-  
-//         // Verifica si el siguiente elemento es un <ul> con la clase 'lista-sub-menus'
-//         if (subMenu && subMenu.classList.contains('lista-sub-menus')) {
-//           // Oculta todos los submenús
-//           document.querySelectorAll('.lista-sub-menus').forEach(menu => {
-//             if (menu !== subMenu) {
-//               menu.style.display = 'none';
-//             }
-//           });
-  
-//           // Alterna la visibilidad del submenú clicado
-//           if (subMenu.style.display === 'block') {
-//             subMenu.style.display = 'none';
-//           } else {
-//             subMenu.style.display = 'block';
-//           }
-//         }
-//       });
-//     });
-  
-//     // Opcional: Cerrar todos los submenús si el clic fue fuera del menú
-//     document.addEventListener('click', (event) => {
-//       if (!event.target.closest('.lista-menus')) {
-//         document.querySelectorAll('.lista-sub-menus').forEach(menu => {
-//           menu.style.display = 'none';
-//         });
-//       }
-//     });
-//   });
-document.addEventListener('DOMContentLoaded', () => {
+    });
 
+    // Cierra el menú cuando se hace clic fuera de él
+    body.addEventListener('click', (event) => {
+        // Verificar si el clic está fuera del menú y el botón hamburguesa
+        if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
+            menu.classList.remove('active');
+            menuToggle.classList.remove('active');
+        }
+    });
+
+
+    
     function manejarMenu(etiqueta) {
         const boton = document.getElementById(etiqueta);
         const boton_submenu = document.getElementsByClassName('redirrecion-pages');
@@ -117,22 +94,6 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
-     // Función para cargar y renderizar un archivo HTML
-     async function cargarContenido() {
-        try {
-            const respuesta = await fetch('footer.html');
-            if (respuesta.ok) {
-                const html = await respuesta.text();
-                document.getElementById('contenido').innerHTML = html;
-            } else {
-                console.error('Error al cargar el archivo:', respuesta.statusText);
-            }
-        } catch (error) {
-            console.error('Error de red:', error);
-        }
-    }
-
-    // Llama a la función para cargar el contenido
-    cargarContenido();
+    
 });
 
